3aac20b46980fba152ca01f213f1cc0c1b932feb
Raman Sailopal <rsailopal@robinsonbrothers.co.uk>
2022-07-01 09:31:55 +0100

    Removed the use of Lynx as a browser, now using globals to show offline documentation

--- code/entrypoint.sh
+++ code/entrypoint.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 apt-get update 
-apt-get install -y make gcc libssl-dev libncurses-dev dialog sysvbanner apache2
-cd /tmp
-wget --no-check-certificate http://invisible-island.net/datafiles/release/lynx-cur.tar.gz
-tar -xvf lynx-cur.tar.gz
-cd lynx2.9.0dev.10
-./configure --with-ssl
-make
-cp lynx /usr/local/bin
-cp /tmp/lynx2.9.0dev.10/lynx.cfg /usr/local/etc
-cp /tmp/lynx2.9.0dev.10/samples/lynx.lss /usr/local/etc
+apt-get install -y make gcc libncurses-dev dialog sysvbanner
 cd /home/yottadb
+/opt/yottadb/current/ydb <<< 'H'
+cp /home/yottadb/*.m /data/r1.34_x86_64/r
+/opt/yottadb/current/ydb <<< 'ZL "ZHELPPROC.m"'
 vers=$(/opt/yottadb/current/ydb <<< 'W $ZV' | grep -v "YDB>")
 banner yottadb > splash.txt
 echo "Version: $vers" >> splash.txt

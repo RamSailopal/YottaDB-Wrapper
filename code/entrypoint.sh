c14c8ddc2d3fa58273afa48ce2e34b6561a13557
Raman Sailopal <rsailopal@robinsonbrothers.co.uk>
2022-07-01 10:06:14 +0100

    No Z in front of HELPPROC routine

--- code/entrypoint.sh
+++ code/entrypoint.sh
@@ -4,12 +4,12 @@ apt-get install -y make gcc libncurses-dev dialog sysvbanner
 cd /home/yottadb
 /opt/yottadb/current/ydb <<< 'H'
 cp /home/yottadb/*.m /data/r1.34_x86_64/r
-/opt/yottadb/current/ydb <<< 'ZL "ZHELPPROC.m"'
+/opt/yottadb/current/ydb <<< 'ZL "HELPPROC.m"'
 vers=$(/opt/yottadb/current/ydb <<< 'W $ZV' | grep -v "YDB>")
 banner yottadb > splash.txt
 echo "Version: $vers" >> splash.txt
 echo "Website: https://yottadb.com" >> splash.txt
 cp splash.txt /opt/yottadb/current/
-cp ydb /usr/local/bin
+cp /home/yottadb/ydb /usr/local/bin
 tail -f /dev/null
 
